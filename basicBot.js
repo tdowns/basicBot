1b89851b815026dda44d4c5552cfd55cb9853b76
tdowns <tdowns@college.harvard.edu>
2015-02-05 16:02:32 -0500

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2534,7 +2534,7 @@
             	    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
             	    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        API.sendChat("/me " + chat.un + " rolled a " + String(Math.floor(Math.random()*100 + 1)))
+                        API.sendChat("/me " + chat.un + " rolled " + String(Math.floor(Math.random()*100 + 1)))
                     }
             	}
             },
