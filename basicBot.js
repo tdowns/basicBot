d3cdb5c88dbaccf2a052fcef7b80baafb88d3b2e
tdowns <tdowns@college.harvard.edu>
2015-02-05 18:03:34 -0500

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -166,7 +166,7 @@
     var botCreatorIDs = [];
 
     var basicBot = {
-        version: "3.5.9",
+        version: "3.6",
         status: false,
         name: "dash_init",
         loggedInID: null,
